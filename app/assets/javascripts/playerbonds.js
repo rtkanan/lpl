84aa11cd31b20b5ab8f8a0de7efbe01da088fba3
Kannah <rtkanan@gmail.com>
2013-12-19 02:16:44 +0530

    * Adding js and css for playerbonds

---
+++ app/assets/javascripts/playerbonds.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$('#playerbond_player, #playerbond_Crushers, #playerbond_Myrmidons, #playerbond_Spartans, #playerbond_Strikers').sortable({
+		connectWith: "ul"
+	});
+});
