c1a87f8c468216b78e180a15fe6360a92f3635e0
Kannah <rtkanan@gmail.com>
2013-12-19 02:09:25 +0530

    * Update to include all jquer ui functions  * app/assets/javascripts/application.js  * app/assets/stylesheets/application.css * Updated to use simple datpicker  * app/assets/javascripts/seasons.js * Updating to include drag and drop feature for playerbonding  * app/views/playerbonds/_form.html.erb

--- app/assets/javascripts/seasons.js
+++ app/assets/javascripts/seasons.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-	$('#season_from, #season_to').datepicker({
-		showOn: "button",
-		buttonImage: "/assets/calendar.gif",
-		buttonImageOnly: true
-	});
+	$('#season_from, #season_to').datepicker();
+//	$('#season_from, #season_to').datepicker({
+//		showOn: "button",
+//		buttonImage: "/assets/calendar.gif",
+//		buttonImageOnly: true
+//	});
 });
